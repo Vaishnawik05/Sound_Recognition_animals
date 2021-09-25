432e30c47c74e91b3ead44e26418daf9c7a63f46
Vaishnawik05 <79893634+Vaishnawik05@users.noreply.github.com>
2021-09-25 16:50:19 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function startClassification(){
+    navigator.mediaDevices.getUserMedia({ audio: true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/v2Q6lyPbS/model.json', modelReady);
+}
+
+function modelReady(){
+    classifier.classify( gotResults);
+}
+
+function gotResults(error, results) {
+    if(error) {
+        console.error(error);
+    }
+}
